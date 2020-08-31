043b9c33ba9459c89688f1ac767484bff9ed3ab4
Gustavo <gustavo.ovalle.zambrano@gmail.com>
2020-08-31 12:45:48 -0400

    segundo commit: widget de titulo y boton iniciar agregados, tambien se ha creado la referencia al widget para ser programado

--- app/src/main/java/com/ovalle/to_do/MainActivity.java
+++ app/src/main/java/com/ovalle/to_do/MainActivity.java
@@ -2,13 +2,26 @@ package com.ovalle.to_do;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // creando las variables que aran referencia a los widget (aspectos visuales de la aplicacion)
+    // crear estas variables en el metodo mainactivity para que asi todos los metodos creados en este puedan acceder a las variables
+    private TextView txtTitulo;
+    private Button btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //referencias para unir variable con los aspectos visuales
+        txtTitulo = findViewById(R.id.txtTitulo); // este metodo hace referencia al aspecto visual
+        btnIniciar = findViewById(R.id.btnIniciar);
+
+        //txtTitulo.setTextColor(Color.BLACK); //este metodo me permite cambiar el color de un texto
     }
 }
