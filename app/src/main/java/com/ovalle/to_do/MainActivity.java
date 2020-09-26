729cc4889d05341066412c1140b47d60b3b7fe26
Gustavo <gustavo.ovalle.zambrano@gmail.com>
2020-09-26 14:57:19 -0300

    Corrección de detalle en la pantalla de inicio de sesion. pantalla de registro finalizada intente a la pantalla de registro realizada.

--- app/src/main/java/com/ovalle/to_do/MainActivity.java
+++ app/src/main/java/com/ovalle/to_do/MainActivity.java
@@ -2,6 +2,7 @@ package com.ovalle.to_do;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +13,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnRegistrer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRegistrer = findViewById(R.id.btnRegistrer);
 
+        btnRegistrer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentRegister = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intentRegister);
+            }
+        });
     }
+
 }
