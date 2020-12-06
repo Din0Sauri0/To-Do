7a08383b7f86481edc3e9841ab422cbaa70fa253
Din0Sauri0 <gustavo.ovalle.zambrano@gmail.com>
2020-12-06 18:17:47 -0300

    Eliminar nota terminado

--- app/src/main/java/com/ovalle/to_do/verNotas.java
+++ app/src/main/java/com/ovalle/to_do/verNotas.java
@@ -1,22 +1,34 @@
 package com.ovalle.to_do;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.ovalle.to_do.Utilidades.Mensaje;
 
-public class verNotas extends AppCompatActivity {
+public class verNotas extends AppCompatActivity implements View.OnClickListener {
     private FirebaseDatabase database;
     private DatabaseReference reference;
     private FirebaseAuth mAuth;
     //Widget
     private TextView txtTituloNota, txtDescripNota, txtCurpoNota;
+    private ImageButton btnCompartir, btnModificar, btnEliminar;
     //Variables
+    String idUsuario;
     String nombreNota;
     String descripcionNota;
     String cuerpoNota;
@@ -26,6 +38,7 @@ public class verNotas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_notas);
         conectarFirebase();
+        idUsuario = mAuth.getCurrentUser().getUid();
         txtTituloNota = findViewById(R.id.txtTituloNota);
         txtDescripNota = findViewById(R.id.txtDescripNota);
         txtCurpoNota = findViewById(R.id.txtCuerpoNota);
@@ -36,6 +49,17 @@ public class verNotas extends AppCompatActivity {
         txtTituloNota.setText(nombreNota);
         txtDescripNota.setText(descripcionNota);
         txtCurpoNota.setText(cuerpoNota);
+        btnCompartir = findViewById(R.id.btnCompartir);
+        btnModificar = findViewById(R.id.btnModificar);
+        btnEliminar = findViewById(R.id.btnEliminar);
+
+        txtCurpoNota.setFocusable(false);
+        txtCurpoNota.setCursorVisible(false);
+        txtCurpoNota.setKeyListener(null);
+
+        btnModificar.setOnClickListener(this);
+        btnCompartir.setOnClickListener(this);
+        btnEliminar.setOnClickListener(this);
 
     }
 
@@ -49,5 +73,51 @@ public class verNotas extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnCompartir:
+                Mensaje.mensajeShort(getApplicationContext(), "Compartir");
+                break;
+            case R.id.btnEliminar:
+                //Mensaje.errorMensaje(getApplicationContext(), "Eliminar");
+                eliminar(idNota);
+
+                break;
+            case R.id.btnModificar:
+                Mensaje.warningMensaje(getApplicationContext(),"Modificar");
+                break;
+        }
+    }
+
+    public void eliminar(final String id){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage("¿Esta seguro que desea eliminar la nota?");
+        dialog.setTitle("Eliminar");
+        dialog.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Mensaje.mensajeShort(getApplicationContext(), "Has presionado si");
+                reference.child("Usuarios").child(idUsuario).child("Tareas").child(id).removeValue(new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                        startActivity(new Intent(verNotas.this, VerTareas.class));
+                        VerTareas ver = new VerTareas();
+                        //Mensaje.warningMensaje(ver.getApplicationContext(), "Se ha eliminado la nota");
+                        finish();
+                    }
+                });
+            }
+        });
+        dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Mensaje.errorMensaje(getApplicationContext(), "Has presionado no");
+            }
+        });
+        dialog.show();
+    }
+
 
 }
